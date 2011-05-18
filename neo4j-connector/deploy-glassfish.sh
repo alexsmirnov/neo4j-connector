e6df1286b0a0ad35b7d7adb612f429850d56076f
Alex Smirnov <alsmirnnov@gmail.com>
2011-05-18 00:22:23 -0700

    deploy connector as XA resource

--- neo4j-connector/deploy-glassfish.sh
+++ neo4j-connector/deploy-glassfish.sh
@@ -6,7 +6,7 @@ ASADM=asadmin
 fi
 $ASADM "$@" <<-EOF
 # create neo4j resource connection
-deploy target/neo4j-connector-0.1-SNAPSHOT.rar
-create-connector-connection-pool --raname=neo4j-connector-0.1-SNAPSHOT --connectiondefinition=com.netoprise.neo4j.connection.Neo4JConnectionFactory  --property=dir=\$\{com.sun.aas.instanceRoot\}/lib/databases/neo4j Neo4jPool
+deploy --force target/neo4j-connector-0.1-SNAPSHOT.rar
+create-connector-connection-pool --raname=neo4j-connector-0.1-SNAPSHOT --connectiondefinition=com.netoprise.neo4j.connection.Neo4JConnectionFactory  --property=xa=true:dir=\$\{com.sun.aas.instanceRoot\}/lib/databases/neo4j Neo4jPool
 create-connector-resource --poolname=Neo4jPool /eis/Neo4j
 EOF
